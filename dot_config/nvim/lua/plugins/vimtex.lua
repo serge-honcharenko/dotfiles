8fd69b11587f86b8a98043a3439023a1e9cdc63d
Serge Honcharenko <serge.honcharenko@icloud.com>
2026-05-09 21:36:14 -0500

    add a build directory for latex compilation garbage

--- dot_config/nvim/lua/plugins/vimtex.lua
+++ dot_config/nvim/lua/plugins/vimtex.lua
@@ -4,5 +4,9 @@ return {
     init = function()
         vim.g.vimtex_view_method = "skim"
         vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
+        vim.g.vimtex_compiler_latexmk = {
+            aux_dir = "./build",
+            out_dir = "./build",
+        }
     end,
 }
